89a719451392f20e5dc0ee7c9a69f55b6145f945
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-18 19:41:51 +0200

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Иван Семикин on 18/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Иван Семикин on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
